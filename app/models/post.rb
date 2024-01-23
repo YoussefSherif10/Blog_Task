3b7a9aa9d045c61c139e86867268bb726c9946d4
Youssef Sherif <youssef98sherif@gmail.com>
2024-01-24 00:49:39 +0200

    Add tags to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,21 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+  validates :tags, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
 
   scope :filter_by_title, lambda { |query| where("lower(title) LIKE ?", "%#{query.downcase}%") }
   scope :sort_by_creation, lambda { order(created_at: :asc) }
   scope :recent, lambda { order(updated_at: :desc) }
+  scope :filter_by_tag, lambda { |tag| where("tags LIKE ?", "%#{tag}%") }
 
   def self.search(params = {})
-    products = Post.all
-    products = products.filter_by_title(params[:title]) if params[:title]
-    products = products.recent if params[:recent]
-    products = products.sort_by_creation if params[:sort_by_creation]
-    products
+    posts = Post.all
+    posts = posts.filter_by_title(params[:title]) if params[:title]
+    posts = posts.filter_by_tag(params[:tag]) if params[:tag]
+    posts = posts.recent if params[:recent]
+    posts = posts.sort_by_creation if params[:sort_by_creation]
+    posts
   end
 end
